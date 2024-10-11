07a650e10555212dcdea7a5e2f225af7f0adb2c6
pperez-a <pperez-a@student.42madrid.com>
2024-10-11 16:31:44 +0200

    .

--- Cursus/libft/ft_lstadd_back_bonus.c
+++ Cursus/libft/ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: pperez-a <pperez-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 11:53:24 by pperez-a          #+#    #+#             */
-/*   Updated: 2024/10/11 16:24:56 by pperez-a         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:31:41 by pperez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,4 @@ void	ft_lstadd_back(t_list **lst, t_list *new_node)
 			temp->next = new_node;
 		}
 	}
-}
-int	main(void)
-{
-	t_list **head;
-
-	head = NULL;	
-	int content = 42;
-	ft_lstadd_back(head, ft_lstnew(&content));
 }
